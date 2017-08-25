cfb0a5a342a3a8849d0abc078ec45d5c6f794b5b
Ryan Chui <ryan.w.chui@gmail.com>
2017-08-25 16:17:34 -0500

    UTF8

--- src/sems-tensorflow.py
+++ src/sems-tensorflow.py
@@ -1,8 +1,8 @@
 import numpy as np
 from modules.DataHandler import readData
 
-phenotypes = readData("./data/16.pheno.2.txt")
+phenotypes = readData("./data/8.pheno.2.txt")
 print("Phenotypes:",  phenotypes.shape, "\n", phenotypes)
 
-snps = readData("./data/16.snps.txt")
+snps = readData("./data/8.snps.txt")
 print("SNPs:",  snps.shape, "\n", snps)
