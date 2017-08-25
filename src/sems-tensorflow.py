cb58e7efa8dfa0a6ce43ee60c6befbf3ae1c567e
Ryan Chui <ryan.w.chui@gmail.com>
2017-08-25 16:10:56 -0500

    updating to utf-16

--- src/sems-tensorflow.py
+++ src/sems-tensorflow.py
@@ -1,8 +1,8 @@
 import numpy as np
 from modules.DataHandler import readData
 
-phenotypes = readData("./data/pheno.2.txt")
+phenotypes = readData("./data/16.pheno.2.txt")
 print("Phenotypes:",  phenotypes.shape, "\n", phenotypes)
 
-snps = readData("./data/snps.txt")
+snps = readData("./data/16.snps.txt")
 print("SNPs:",  snps.shape, "\n", snps)
