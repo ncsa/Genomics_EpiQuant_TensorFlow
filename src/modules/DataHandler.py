1f3af86ce227d296d6135d9d849bf2ffbcf36dea
Ryan Chui <ryan.w.chui@gmail.com>
2017-08-25 15:45:01 -0500

    utf 16 encoding

--- src/modules/DataHandler.py
+++ src/modules/DataHandler.py
@@ -2,11 +2,12 @@ import numpy as np
 import codecs
 
 def readData(filePath):
-    with codecs.open(filePath) as file:
+    with codecs.open(filePath, 'r', encoding=('utf-8')) as file:
         # Reads in data from a file.
         # Strips white space and newlines from the ends.
         # Splits by newlines.
         # For each split, split by tabs.
         # Delete the first column.
         # Convert array to floating point.
-        return np.delete([x.split("\t") for x in file.read().strip().split("\n")], 0, 1).astype(np.float)
+        print([x.split("\t") for x in file.read().strip().split("\n")])
+        # return np.delete([x.split("\t") for x in file.read().strip().split("\n")], 0, 1).astype(np.float)
