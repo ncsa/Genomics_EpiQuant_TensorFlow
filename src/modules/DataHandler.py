4ca36d44b9317a7783d4a9f1affc06a6dc998051
Ryan Chui <ryan.w.chui@gmail.com>
2017-08-25 23:38:21 -0500

    Added data generation

--- src/modules/DataHandler.py
+++ src/modules/DataHandler.py
@@ -14,23 +14,31 @@ def readData(filePath):
         dataArray = np.delete(fullArray, 0, 1).astype(np.float)
         return nameArray, dataArray
 
-def nameGeneration(name1, name2, position, outputArray):
+def generateNames(name1, name2, position, outputArray):
     outputArray[position] = ["%s%s%s" % (name1, "::", name2)]
-    print(position)
+
+def generateData(array1, array2, position, outputArray):
+    outputArray[position] = np.multiply(array1, array2)
 
 def calculateInteractions(inputArray):
     length = len(inputArray)
     combinations = length + (math.factorial(length) // 2 // math.factorial(length - 2))
     iteration = length
-    print(combinations)
-    if isinstance(inputArray[0][0], str):
+    isString = isinstance(inputArray[0][0], str)
+    outputArray = None
+
+    if isString:
         outputArray = np.empty([combinations, 1], dtype=object)
-        for i in range(length):
-            outputArray[i] = inputArray[i]
-        for i in range(length):
-            for j in range(length - (i + 1)):
-                nameGeneration(inputArray[i][0], inputArray[length - j - 1][0], iteration, outputArray)
-                iteration += 1
-        print(outputArray.shape)
     else:
-        print(inputArray[0][0])
+        outputArray = np.empty([combinations, len(inputArray[0])], dtype=object)
+
+    for i in range(length):
+        outputArray[i] = inputArray[i]
+    for i in range(length):
+        for j in range(length - (i + 1)):
+            if isString:
+                generateNames(inputArray[i][0], inputArray[length - j - 1][0], iteration, outputArray)
+            else:
+                generateData(inputArray[i], inputArray[length - j - 1], iteration, outputArray)
+            iteration += 1
+    print(outputArray, outputArray.shape)
--- src/sems-tensorflow.py
+++ src/sems-tensorflow.py
@@ -1,14 +1,15 @@
 import numpy as np
 import modules.DataHandler as dh
 
+print()
 phenoNames, phenoData = dh.readData("./data/8.pheno.2.txt")
-print("Phenotypes:", phenoData.shape, "\n", phenoNames, "\n", phenoData, "\n")
+print("Phenotypes:", phenoData.shape, "\n\n", phenoNames, "\n\n", phenoData, "\n")
 
 snpNames, snpData = dh.readData("./data/8.snps.txt")
-print("SNPs:", snpData.shape, "\n", snpNames, "\n", snpData, "\n")
+print("SNPs:", snpData.shape, "\n\n", snpNames, "\n\n", snpData, "\n")
 
-dh.calculateInteractions(phenoNames)
-# dh.calculateInteractions(phenoData)
+# dh.calculateInteractions(phenoNames)
+dh.calculateInteractions(phenoData)
 
-dh.calculateInteractions(snpNames)
-# dh.calculateInteractions(snpData)
+# dh.calculateInteractions(snpNames)
+dh.calculateInteractions(snpData)
