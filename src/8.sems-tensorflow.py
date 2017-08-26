a37679b58822b2a62fe9dce37fac9d023f35046a
Ryan Chui <ryan.w.chui@gmail.com>
2017-08-26 00:08:46 -0500

    Deleting garbage files

--- src/8.sems-tensorflow.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from modules.DataHandler import readData
-
-phenotypes = readData("./data/16.pheno.2.txt")
-print("Phenotypes:",  phenotypes.shape, "\n", phenotypes)
-
-snps = readData("./data/16.snps.txt")
-print("SNPs:",  snps.shape, "\n", snps)
