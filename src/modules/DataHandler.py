324d34fe7a9d3b8b5bbb5fba6585c7362cb57a0b
Ryan Chui <ryan.w.chui@gmail.com>
2017-08-26 00:04:13 -0500

    one stepping data retreival

--- src/modules/DataHandler.py
+++ src/modules/DataHandler.py
@@ -1,26 +1,43 @@
 import numpy as np
-import _thread
 import math
 
-def readData(filePath):
-    with open(filePath) as file:
-        # Reads in data from a file.
-        # Strips white space and newlines from the ends.
-        # Splits by newlines then tabs for each split.
-        # Slice off name column and delete name column from data array.
-        # Convert data array to floating point.            
-        fullArray = np.asarray([x.split("\t") for x in file.read().strip().split("\n")])
-        nameArray = np.asarray([[x] for x in fullArray[:, 0]])
-        dataArray = np.delete(fullArray, 0, 1).astype(np.float)
-        return nameArray, dataArray
-
 def generateNames(name1, name2, position, outputArray):
+    """ Generates all possible name combinations.
+
+    Args:
+        name1: The first name to concatenate.
+        name2: The second name to concatenate.
+        position: The position to place names.
+        ouputArray: Contains all name combinations.
+
+    Returns:
+        None
+    """
     outputArray[position] = ["%s%s%s" % (name1, "::", name2)]
 
 def generateData(array1, array2, position, outputArray):
+    """ Generates all possible data combinations.
+
+    Args:
+        array1: The first array of data points.
+        array2: The second array of data points.
+        position: The position to place data.
+        outputArray: Contains all data point combinations.
+
+    Returns:
+        None
+    """
     outputArray[position] = np.multiply(array1, array2)
 
 def calculateInteractions(inputArray):
+    """ Calculates all pairwise epistatic interactions.
+    
+    Args:
+        inputArray: Contains all single epistatic contributions.
+
+    Returns:
+        outputArray: Contains all single and pairwise interaction contributions.
+    """
     length = len(inputArray)
     combinations = length + (math.factorial(length) // 2 // math.factorial(length - 2))
     iteration = length
@@ -41,4 +58,24 @@ def calculateInteractions(inputArray):
             else:
                 generateData(inputArray[i], inputArray[length - j - 1], iteration, outputArray)
             iteration += 1
-    print(outputArray, outputArray.shape)
+
+def getData(filePath):
+    """ Reads in data from a file and formats it into a name and data array.
+
+    Args:
+        filePath: The path to the file to be read in.
+
+    Returns:
+        nameArray: Contains the names of each data row.
+        dataArray: Contains the data elements of each row.
+    """
+    with open(filePath) as file:
+        # Reads in data from a file.
+        # Strips white space and newlines from the ends.
+        # Splits by newlines then tabs for each split.   
+        fullArray = np.asarray([x.split("\t") for x in file.read().strip().split("\n")])
+        # Slice off name column and delete name column from data array.    
+        nameArray = np.asarray([[x] for x in fullArray[:, 0]])
+        # Convert data array to floating point.
+        dataArray = np.delete(fullArray, 0, 1).astype(np.float)
+        return calculateInteractions(nameArray), calculateInteractions(dataArray)
--- src/sems-tensorflow.py
+++ src/sems-tensorflow.py
@@ -2,14 +2,8 @@ import numpy as np
 import modules.DataHandler as dh
 
 print()
-phenoNames, phenoData = dh.readData("./data/8.pheno.2.txt")
+phenoNames, phenoData = dh.getData("./data/8.pheno.2.txt")
 print("Phenotypes:", phenoData.shape, "\n\n", phenoNames, "\n\n", phenoData, "\n")
 
-snpNames, snpData = dh.readData("./data/8.snps.txt")
+snpNames, snpData = dh.getData("./data/8.snps.txt")
 print("SNPs:", snpData.shape, "\n\n", snpNames, "\n\n", snpData, "\n")
-
-# dh.calculateInteractions(phenoNames)
-dh.calculateInteractions(phenoData)
-
-# dh.calculateInteractions(snpNames)
-dh.calculateInteractions(snpData)
