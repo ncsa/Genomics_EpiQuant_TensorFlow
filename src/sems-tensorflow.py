29684374605686f16bbd621ed8dbe6912c3720b9
Ryan Chui <ryan.w.chui@gmail.com>
2017-08-28 11:37:56 -0500

    Defining getCombinations boolean

--- src/sems-tensorflow.py
+++ src/sems-tensorflow.py
@@ -4,16 +4,16 @@ import modules.DataHandler as dh
 import modules.Network as net
 
 print()
-phenoNames, phenoData = dh.getData("./data/8.pheno.2.txt")
+phenoNames, phenoData = dh.getData("./data/8.pheno.2.txt", False)
 print("Phenotypes:", phenoData.shape, "\n\n", phenoNames, "\n\n", phenoData, "\n")
 
-snpNames, snpData = dh.getData("./data/8.snps.txt")
+snpNames, snpData = dh.getData("./data/8.snps.txt", True)
 print("SNPs:", snpData.shape, "\n\n", snpNames, "\n\n", snpData, "\n")
 
 size = len(snpData[0])
 
 iLayer = net.InputLayer(size)
-cLayer = net.ConnectedLayer(size)
+cLayer = net.ConnectedLayer(size, iLayer.il)
 
 iLayer.shape()
 cLayer.shape()
