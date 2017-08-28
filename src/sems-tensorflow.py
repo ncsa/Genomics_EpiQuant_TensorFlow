6652c9b95d1f069e43a6bb8603f91ff3b9c3de7e
Ryan Chui <ryan.w.chui@gmail.com>
2017-08-28 14:04:37 -0500

    Tranposing matrices

--- src/sems-tensorflow.py
+++ src/sems-tensorflow.py
@@ -8,6 +8,8 @@ phenoNames, phenoData = dh.getData("./data/8.pheno.2.txt", False)
 print("Phenotypes:", phenoData.shape, "\n\n", phenoNames, "\n\n", phenoData, "\n")
 
 snpNames, snpData = dh.getData("./data/8.snps.txt", True)
+snpNames = np.transpose(snpNames)
+snpData = np.tranpose(snpData)
 print("SNPs:", snpData.shape, "\n\n", snpNames, "\n\n", snpData, "\n")
 
 size = len(snpData[0])
