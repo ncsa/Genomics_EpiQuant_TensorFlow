39b52d2d1ec536c9ec674125828a01ac12ddc63f
Ryan Chui <ryan.w.chui@gmail.com>
2017-08-29 21:34:10 -0500

    Padding training log

--- src/modules/SessionHandler.py
+++ src/modules/SessionHandler.py
@@ -16,9 +16,9 @@ def progress(i, length, message):
 def logTraining(pastLoss, currentLoss, alpha):
     print(
         "\n",
-        "Loss:", "{:.2E}".format(currentLoss),
-        "Delta:", "{:.2E}".format(abs(pastLoss-currentLoss)),
-        "Alpha:", "{:.2E}".format(alpha),
+        "   Loss:", "{:.2E}".format(currentLoss),
+        "   Delta:", "{:.2E}".format(abs(pastLoss-currentLoss)),
+        "   Alpha:", "{:.2E}".format(alpha),
         end="\r"
     )
 
