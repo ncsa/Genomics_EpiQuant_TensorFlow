f58e6e470ec74ec774829b63c267852bc9188dad
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-13 14:59:26 -0500

    Convert all levels of outputArray to np arrays

--- src/modules/BatchBuilder.py
+++ src/modules/BatchBuilder.py
@@ -16,9 +16,14 @@ def makeBatches(inputArray, size):
     print(length, size)
     batches = length // size + 1
     print(batches)
+    
     outputArray = np.array_split(inputArray, batches)
     for i in range(len(outputArray)):
+        for j in range(len(outputArray[i])):
+            outputArray[i][j] = np.asarray(outputArray[i][j])
         outputArray[i] = np.asarray(outputArray[i])
+    outputArray = np.asarray(outputArray
+    )
     print(outputArray)
     print(outputArray.shape)
 
