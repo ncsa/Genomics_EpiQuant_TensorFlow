4ac1f10f88f04f80a3a29b3944431924014e3aa8
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-14 17:40:38 -0500

    Testing makeBatches entry

--- src/modules/BatchBuilder.py
+++ src/modules/BatchBuilder.py
@@ -15,6 +15,7 @@ def makeBatches(inputArray, batches):
     length = len(inputArray)
 
     outputArray = np.asarray(np.array_split(inputArray, batches))
+    print("Making Batches...")
     for i in range(len(outputArray)):
         prog.progress(i, len(outputArray), "Batches Built")
         outputArray[i] = np.asarray(outputArray[i])
