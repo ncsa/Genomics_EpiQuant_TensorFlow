7fe5a241d77729fb00065ff2e5711800477a937d
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-14 23:56:40 -0500

    0.0005 training rate

--- src/modules/BatchBuilder.py
+++ src/modules/BatchBuilder.py
@@ -15,16 +15,14 @@ def makeBatches(inputArray, batches):
     length = len(inputArray)
 
     outputArray = np.asarray(np.array_split(inputArray, batches))
-    print(outputArray)
-
-    print("Making Batches...")
+    
     for i in range(len(outputArray)):
         prog.progress(i, len(outputArray), "Batches Built")
         outputArray[i] = np.asarray(outputArray[i])
         for j in range(len(outputArray[i])):
             outputArray[i][j] = np.asarray(outputArray[i][j])
 
-    print(outputArray.shape)
-    print(outputArray[0].shape)
+    # print(outputArray.shape)
+    # print(outputArray[0].shape)
 
     return outputArray
