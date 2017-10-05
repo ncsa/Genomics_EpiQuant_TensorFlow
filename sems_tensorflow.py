ecd91973b75f09236675f6c546048fb411421ced
Ryan Chui <ryan.w.chui@gmail.com>
2017-10-05 10:55:15 -0500

    print hostnames

--- sems_tensorflow.py
+++ sems_tensorflow.py
@@ -6,11 +6,13 @@ Main driver script for stepwise epistatic model selection.
 import os
 
 PBS_JOBID = os.environ['PBS_JOBID']
+PBS_HOSTNAMES = '/var/spool/torque/aux/' + PBS_JOBID
 
 def main():
     """ Main of sems_tensorflow """
-    print(PBS_JOBID)
-    print('/var/spool/torque/aux/' + PBS_JOBID)
+    with open(PBS_HOSTNAMES) as host_file:
+        host_names = [x for x in host_file.read()]
+    print(host_names)
 
 if __name__ == "__main__":
     main()
