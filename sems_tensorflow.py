6771d3b7fac089268a2cbf148489a481814122ed
Ryan Chui <ryan.w.chui@gmail.com>
2017-10-05 13:35:58 -0500

    Pop off master node

--- sems_tensorflow.py
+++ sems_tensorflow.py
@@ -11,8 +11,9 @@ PBS_HOSTNAMES = '/var/spool/torque/aux/' + PBS_JOBID
 def main():
     """ Main of sems_tensorflow """
     with open(PBS_HOSTNAMES) as host_file:
-        host_names = [(x.strip() + ':2222') for x in host_file.readlines()]
-    print(host_names)
+        workers = [(x.strip() + ':2222') for x in host_file.readlines()]
+    master = [workers.pop()]
+    print(master, workers)
 
 if __name__ == "__main__":
     main()
