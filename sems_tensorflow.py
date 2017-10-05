9660f7fbef616ac5d4e95d194cc0d0085e716d1d
Ryan Chui <ryan.w.chui@gmail.com>
2017-10-05 13:32:43 -0500

    host_file output

--- sems_tensorflow.py
+++ sems_tensorflow.py
@@ -11,8 +11,9 @@ PBS_HOSTNAMES = '/var/spool/torque/aux/' + PBS_JOBID
 def main():
     """ Main of sems_tensorflow """
     with open(PBS_HOSTNAMES) as host_file:
-        host_names = [(x.strip() + ':2222') for x in (host_file.readlines().split())]
-    print(host_names)
+        print(host_file.readlines())
+        # host_names = [(x.strip() + ':2222') for x in (host_file.readlines().split())]
+    # print(host_names)
 
 if __name__ == "__main__":
     main()
