ebd19683f9ec606688ae6a5dc799657357cd5375
Ryan Chui <ryan.w.chui@gmail.com>
2017-10-05 10:50:39 -0500

    New branch

--- sems_tensorflow.py
+++ sems_tensorflow.py
@@ -1,1 +1,15 @@
-src/SemsTensorFlow.py
+""" sems_tensorflow.py
+
+Main driver script for stepwise epistatic model selection.
+"""
+
+import os
+
+PBS_JOBID = os.environ['PBS_JOBID']
+
+def main():
+    print(PBS_JOBID)
+    print('/var/spool/torque/aux/' + PBS_JOBID)
+
+if __name__ == "__main__":
+    main()
